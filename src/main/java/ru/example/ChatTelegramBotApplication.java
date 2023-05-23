157cafeaa3a88347db2bb9a6ea3480732e0357a8
Ramones <roman.sercent@gmail.com>
2023-05-23 21:31:34 +0300

    init?

--- src/main/java/ru/example/ChatTelegramBotApplication.java
+++ src/main/java/ru/example/ChatTelegramBotApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleTelegramBotApplication {
+public class ChatTelegramBotApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SimpleTelegramBotApplication.class, args);
+        SpringApplication.run(ChatTelegramBotApplication.class, args);
     }
 }
